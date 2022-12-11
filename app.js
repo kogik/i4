c53d472c9612e39f2ce259cf5dba8616ee87028a
kogik <klikmailkogik@gmail.com>
2022-12-11 05:32:51 +0100

    added mongoose, user router deleted

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+
+require('dotenv').config()
+
+mongoose.set('strictQuery', true)
+mongoose.connect("mongodb+srv://i4test:" + process.env.MONGODB_PASS + "@cluster0.i8av4uz.mongodb.net/", {useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("[DB] > connected"))
+  .catch(err => console.log(err))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +26,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
