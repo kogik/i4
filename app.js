79df836c067cbbf2b6cc099e9dc071a2c82e10a4
kogik <klikmailkogik@gmail.com>
2022-12-11 12:25:16 +0100

    login system - terrible design but working

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var indexRouter = require('./routes/index');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var session = require('express-session')
+var logger = require('morgan')
+var mongoose = require('mongoose')
+var {User, UserSchema} = require('./models/user')
+var passport = require('passport')
 
-var app = express();
+
+
+
+var indexRouter = require('./routes/index')
+const { env } = require('process')
+
+var app = express()
 
 require('dotenv').config()
+
 
 mongoose.set('strictQuery', true)
 mongoose.connect("mongodb+srv://i4test:" + process.env.MONGODB_PASS + "@cluster0.i8av4uz.mongodb.net/", {useNewUrlParser: true, useUnifiedTopology: true })
@@ -16,31 +24,69 @@ mongoose.connect("mongodb+srv://i4test:" + process.env.MONGODB_PASS + "@cluster0
   .catch(err => console.log(err))
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// morgan middleware
+app.use(logger('dev'))
 
-app.use('/', indexRouter);
+// middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(session({
+  secret: env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+
+// Passport.js
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated()) return next();
+	res.redirect('/login');
+}
+
+function isLoggedOut(req, res, next) {
+	if (!req.isAuthenticated()) return next();
+	res.redirect('/');
+}
+
+
+
+app.use('/test', (req, res, next) => {
+  console.log(User);
+  res.send("check console");
+})
+
+app.use('/', indexRouter)
+
+
+// User.register({username: 'jozo'}, '123', (err)=>{
+//   if (err) console.log(err)
+// })
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
-var router = express.Router();
+const createHttpError = require('http-errors');
+var passport = require('passport')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'i4' });
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'i4 - login' });
+});
+
+router.get('/dashboard',isLoggedIn, function(req, res, next) {
+  res.render('dashboard', { title: 'i4 - dashboard', user: req.user.username });
+});
+
+router.post("/login", passport.authenticate('local', {
+  successRedirect: "/dashboard",
+  failureRedirect: "/login",
+}))
+
+
+router.get('/logout', function(req, res, next) {
+  req.logout((err)=>{
+    if (err) console.log(err)
+  })
+  res.redirect('/')
+});
+
+
+
+
+
+
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated()) return next();
+	res.redirect('/login');
+}
+
+function isLoggedOut(req, res, next) {
+	if (!req.isAuthenticated()) return next();
+	res.redirect('/');
+}
+
+
 
 module.exports = router;
