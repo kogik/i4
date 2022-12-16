24699f0c5e86cba101f4bed8cf9d12b13b59539a
kogik <klikmailkogik@gmail.com>
2022-12-16 02:48:29 +0100

    small fixes, admin panel user list added

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var createHttpError = require('http-errors');
-var passport = require('passport')
+var passport = require('passport');
+const { User } = require('../models/user');
 var router = express.Router()
 
 /* GET home page. */
@@ -9,13 +10,16 @@ router.get('/dashboard', function(req, res, next) {
     res.render('user/dashboard', { title: 'i4 - dashboard', user: req.user });
 });
 
-router.get('/admin-panel', isAdmin, function(req, res, next) {
-    res.render('user/admin-panel', { title: 'i4 - dashboard', user: req.user });
+router.get('/admin-panel', isAdmin,async function(req, res, next) {
+    User.find({},(err, result) =>{
+        if (err) console.log(err)
+        res.render('user/admin-panel', { title: 'i4 - dashboard', user: req.user, users: result });
+    })
 });
 
 function isAdmin(req, res, next){
-    if(req.user.role == "admin") next()
-    res.redirect("/user/dashboard")
+    if(req.user.role == "admin") return next()
+    return res.redirect("/user/dashboard")
 }
 
 
